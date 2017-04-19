6aadc54126279cd516b6d3bc81630dfb3b954870
Joshua Borwey <joshua.borwey@gmail.com>
2017-04-19 15:37:57 -0500

    Work on fixing test cases.  Fix function for mines at the edge of row/column

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -8,7 +8,14 @@ public class Minesweeper {
 
     public static char[][] updateBoard(char[][] board, int[] click) {
         int height = board.length, width = board[0].length;
+
+        if(board[click[0]][click[1]] == 'M'){
+            board[click[0]][click[1]] = 'X';
+            return board;
+        }
+
         List<int[]> mineList = new ArrayList<>();
+
 
         for(int i=0; i<height; i++){
 
@@ -20,28 +27,29 @@ public class Minesweeper {
                     int[] mine = {i, j};
                     mineList.add(mine);
 
-                    foundMine(board, i, j, -1, 1, height, width);
-                    foundMine(board, i, j, 0, 1, height, width);
-                    foundMine(board, i, j, 1, 1, height, width);
+                    foundMine(board, i, j, -1, -1, height, width);
+                    foundMine(board, i, j, 0, -1, height, width);
+                    foundMine(board, i, j, 1, -1, height, width);
+                }
+                else if(board[i][j] == 'B'){
+                    break;
                 }
             }
         }
 
         for(int[] mine : mineList){
-            int rMine = mine[0];
-            int cMine = mine[1];
+            int rMine = mine[0], cMine = mine[1];
             checkAdjacent(board, rMine, cMine, height, width);
-            if(rMine == click[0] && cMine == click[1]){
-                board[rMine][cMine] = 'X';
-            }
+
         }
 
         return board;
     }
 
     public static void foundMine(char[][] board, int row, int column, int rowOffset, int columnOffset, int height, int width){
-        int iNeighbor = row - rowOffset;
-        int jNeighbor = column - columnOffset;
+        int iNeighbor = row + rowOffset;
+        int jNeighbor = column + columnOffset;
+        int columnEnd = column + 1;
         if(iNeighbor < 0 || iNeighbor >= height){
             return;
         }
@@ -49,11 +57,11 @@ public class Minesweeper {
             jNeighbor += 1;
         }
 
-        if(column == width){
-            column -= 1;
+        if(columnEnd == width){
+            columnEnd -= 1;
         }
 
-        for(int j=jNeighbor; j<column+1; j++){
+        for(int j=jNeighbor; j<=columnEnd; j++){
             char tile = board[iNeighbor][j];
             if(tile == 'E' || tile == 'B'){
                 board[iNeighbor][j] = '1';
@@ -76,6 +84,34 @@ public class Minesweeper {
             columnStart += 1;
         }
 
+        if(rowEnd >= height){
+            rowEnd -= 1;
+        }
+
+        if(columnEnd >= width){
+            columnEnd -= 1;
+        }
+        for(int i=rowStart; i<rowEnd; i++){
+            for(int j=columnStart; j<=columnEnd; j++){
+                if(board[i][j] != 'M' && !adjacentSpace(board, i, j, height, width)){
+                    board[i][j] = 'E';
+                }
+            }
+        }
+
+    }
+    public static boolean adjacentSpace(char[][] board, int row, int column, int height, int width){
+        int rowStart = row - 1; int rowEnd = row + 1;
+        int columnStart = column - 1; int columnEnd = column + 1;
+
+        if(rowStart < 0){
+            rowStart += 1;
+        }
+
+        if(columnStart < 0){
+            columnStart += 1;
+        }
+
         if(rowEnd > height){
             rowEnd -= 1;
         }
@@ -83,58 +119,15 @@ public class Minesweeper {
         if(columnEnd == width){
             columnEnd -= 1;
         }
-        for(int i=rowStart; i<rowEnd; i++){
-            for(int j=columnStart; j<columnEnd; j++){
-                if(!adjacentNorth(board, i, j) && !adjacentEast(board, i, j, width) &&
-                        !adjacentSouth(board, i, j, height) && !adjacentWest(board, i, j) && board[i][j] != 'M'){
-                    board[i][j] = 'E';
+
+        for(int i=rowStart; i<=rowEnd; i++){
+            for(int j=columnStart; j<=columnEnd; j++){
+                if(board[i][j] == 'B'){
+                      return true;
                 }
             }
         }
-
-    }
-    public static boolean adjacentNorth(char[][] board, int row, int column){
-        if(row-1 < 0){
-            return false;
-        }
-        else if(board[row-1][column] == 'M' || board[row-1][column] != 'B'){
-            return false;
-        }
-
-        return true;
-    }
-
-    public static boolean adjacentEast(char[][] board, int row, int column, int width){
-        if(column + 1 >= width){
-            return false;
-        }
-        else if(board[row][column+1] == 'M' || board[row][column + 1] != 'B'){
-            return false;
-        }
-
-        return true;
-    }
-
-    public static boolean adjacentSouth(char[][] board, int row, int column, int height){
-        if(row+1 >= height){
-            return false;
-        }
-        else if(board[row+1][column] == 'M' || board[row+1][column] != 'B'){
-            return false;
-        }
-
-        return true;
-    }
-
-    public static boolean adjacentWest(char[][] board, int row, int column){
-        if(column - 1 < 0){
-            return false;
-        }
-        else if(board[row][column-1] == 'M' || board[row][column-1] != 'B'){
-            return false;
-        }
-
-        return true;
+        return false;
     }
 
     public static void main(String[] args) {
@@ -145,18 +138,63 @@ public class Minesweeper {
 //                    {'E', 'E', 'E', 'E', 'E'}   };
 //
 //        int[] click = {3, 0};
+//
+//        char[][] minefield =
+//                {       {'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'},
+//                        {'E', 'E', 'E', 'E', 'E', 'E', 'E', 'M'},
+//                        {'E', 'M', 'E', 'E', 'E', 'E', 'E', 'E'},
+//                        {'M', 'E', 'E', 'E', 'E', 'E', 'E', 'E'},
+//                        {'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'},
+//                        {'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'},
+//                        {'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'},
+//                        {'E', 'E', 'M', 'M', 'E', 'E', 'E', 'E'} };
+//
+//        int[] click = {0, 0};
+
+//        char[][] minefield =
+//                {       {'B', '1', 'E', '1', 'B'},
+//                        {'B', '1', 'M', '1', 'B'},
+//                        {'B', '1', '1', '1', 'B'},
+//                        {'B', 'B', 'B', 'B', 'B'},  };
+//
+//        int[] click = {1, 2};
+
+//                char[][] minefield =
+//                {       {'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'},
+//                        {'E', 'E', 'E', 'E', 'E', 'E', 'E', 'M'},
+//                        {'E', 'E', 'M', 'E', 'E', 'E', 'E', 'E'},
+//                        {'M', 'E', 'E', 'E', 'E', 'E', 'E', 'E'},
+//                        {'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'},
+//                        {'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'},
+//                        {'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'},
+//                        {'E', 'E', 'M', 'M', 'E', 'E', 'E', 'E'} };
+//
+//        int[] click = {0, 0};
+
+        // Line 115: java.lang.ArrayIndexOutOfBoundsException: 8
+//        char[][] minefield =
+//                {       {'E', 'E', 'E', 'E', 'E', 'M', 'M', 'E'},
+//                        {'E', 'E', 'E', 'E', 'E', 'E', 'M', 'E'},
+//                        {'E', 'E', 'E', 'E', 'E', 'E', 'M', 'E'},
+//                        {'E', 'E', 'E', 'E', 'E', 'E', 'M', 'E'},
+//                        {'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'},
+//                        {'E', 'M', 'M', 'E', 'E', 'E', 'E', 'E'},
+//                        {'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'},
+//                        {'E', 'E', 'E', 'E', 'M', 'M', 'E', 'M'} };
+//
+//        int[] click = {2, 2};
 
         char[][] minefield =
-                {       {'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'},
-                        {'E', 'E', 'E', 'E', 'E', 'E', 'E', 'M'},
-                        {'E', 'M', 'E', 'E', 'E', 'E', 'E', 'E'},
-                        {'M', 'E', 'E', 'E', 'E', 'E', 'E', 'E'},
-                        {'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'},
-                        {'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'},
-                        {'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'},
-                        {'E', 'E', 'M', 'M', 'E', 'E', 'E', 'E'} };
+                {       {'B', 'B', 'B', 'B', 'B', 'B', '1', 'E'},
+                        {'B', '1', '1', '1', 'B', 'B', '1', 'M'},
+                        {'1', '2', 'M', '1', 'B', 'B', '1', '1'},
+                        {'M', '2', '1', '1', 'B', 'B', 'B', 'B'},
+                        {'1', '1', 'B', 'B', 'B', 'B', 'B', 'B'},
+                        {'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B'},
+                        {'B', '1', '2', '2', '1', 'B', 'B', 'B'},
+                        {'B', '1', 'M', 'M', '1', 'B', 'B', 'B'} };
 
-        int[] click = {0, 0};
+        int[] click = {0, 7};
 
         char[][] playedGame = updateBoard(minefield, click);
         for(char[] i : playedGame){
